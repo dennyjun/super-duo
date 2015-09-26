1b30e5fcb13cca8df9d126285573139063876f93
Denny <djguitarstyle@gmail.com>
2015-09-25 23:12:46 -0700

    Fix null pointer exception for authors

--- alexandria/src/main/java/it/jaschke/alexandria/BookDetail.java
+++ alexandria/src/main/java/it/jaschke/alexandria/BookDetail.java
@@ -114,6 +114,9 @@ public class BookDetail extends Fragment implements LoaderManager.LoaderCallback
         ((TextView) rootView.findViewById(R.id.fullBookDesc)).setText(desc);
 
         String authors = data.getString(data.getColumnIndex(AlexandriaContract.AuthorEntry.AUTHOR));
+        if(authors == null) {
+            authors = "";                                                                           // Fix null pointer exception
+        }
         String[] authorsArr = authors.split(",");
         ((TextView) rootView.findViewById(R.id.authors)).setLines(authorsArr.length);
         ((TextView) rootView.findViewById(R.id.authors)).setText(authors.replace(",", "\n"));
